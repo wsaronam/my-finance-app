217604a8ed2ec30e02a66b11cb2b9b8430548c76
Willy Saronamihardja <wsaronam@gmail.com>
2025-01-24 18:46:15 -0800

    updates
    - updated dashboard.html
    - changed home.html to base.html and some updates
    - added basic login.html
    - updated settings.py

--- my_finance_app/urls.py
+++ my_finance_app/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('finance.urls')), # link to the finance app
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
