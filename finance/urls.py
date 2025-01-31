300932a8af96c212768e54646899251068d1fa43
Willy Saronamihardja <wsaronam@gmail.com>
2025-01-31 02:29:09 -0800

    updates
    - fixed style.css bug
    - added logout functionality
    - added error message if log-in was incorrect
    - added sign-up button to login screen

--- finance/urls.py
+++ finance/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
     path('add/', views.add_transaction, name='add_transaction'),
     path('accounts/', include('django.contrib.auth.urls')),
